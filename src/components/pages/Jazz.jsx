f67735a4130b9b6078a8093d0eb82a74314d82d2
kalikokalikova <calico.seders@gmail.com>
2025-09-18 10:05:47 -0700

    first commit

---
+++ src/components/pages/Jazz.jsx
@@ -0,0 +1,5 @@
+export default function Jazz() {
+  return (
+    <div>Jazz</div>
+  )
+}
---
+++ src/main.jsx
@@ -0,0 +1,36 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router";
+import "./index.css";
+import App from "./App.jsx";
+
+import Home from "./components/pages/Home.jsx";
+import About from "./components/pages/About.jsx";
+import Jazz from "./components/pages/Jazz.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "about",
+        element: <About />,
+      },
+      {
+        path: "jazz",
+        element: <Jazz />
+      }
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+);
