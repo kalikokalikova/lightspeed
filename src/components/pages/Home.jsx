585edd6005c75116e227f35379a75ec00951a31d
kalikokalikova <calico.seders@gmail.com>
2025-09-18 11:55:32 -0700

    header looking pretty good

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,16 +1,31 @@
 import backgroundImage from "../../assets/darkwater.jpg";
+import { Box, Typography } from "@mui/material";
 
 export default function Home() {
-
   const backgroundStyle = {
     backgroundImage: `url(${backgroundImage})`,
-    backgroundSize: 'cover',
-    backgroundPosition: 'center',
-    backgroundRepeat: 'no-repeat',
-    height: '100vh' // Example height
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat",
+    height: "100vh",
+    padding: "10px 20px"
   };
 
   return (
-    <div style={backgroundStyle}>Home</div>
-  )
+    <Box sx={backgroundStyle}>
+      <Box sx={{ display: "flex", flexDirection: "column", alignItems: "flex-end", textTransform: "uppercase" }}>
+        <Typography
+          variant="h1"
+          className="rubik"
+        >
+          Calico Seders
+        </Typography>
+        <Typography variant="h4" className="ubuntu-sans-mono-regular">
+          hacker | artist | activist | nerd
+        </Typography>
+      </Box>
+
+      <Box>Home</Box>
+    </Box>
+  );
 }
