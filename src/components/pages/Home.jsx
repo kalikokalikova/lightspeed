8cbc2a76b16c292b0b4ae24a57ed9a672b0bab6b
kalikokalikova <calico.seders@gmail.com>
2025-09-18 11:07:34 -0700

    css reset, background images

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,5 +1,16 @@
+import backgroundImage from "../../assets/darkwater.jpg";
+
 export default function Home() {
+
+  const backgroundStyle = {
+    backgroundImage: `url(${backgroundImage})`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    backgroundRepeat: 'no-repeat',
+    height: '100vh' // Example height
+  };
+
   return (
-    <div>Home</div>
+    <div style={backgroundStyle}>Home</div>
   )
 }
