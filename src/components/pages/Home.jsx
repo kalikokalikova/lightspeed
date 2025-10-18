a73efb40c03ceae2afb915b16460cacc4af31959
kalikokalikova <calico.seders@gmail.com>
2025-10-17 19:00:48 -0700

    home page is responsive

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -4,7 +4,7 @@ import codeImage from "../../assets/code.jpg";
 import aboutImage from "../../assets/mugshot.jpg";
 import loveImage from "../../assets/love.jpg";
 import { Box, Typography, Avatar } from "@mui/material";
-import { Link } from 'react-router';
+import { Link } from "react-router";
 import NavDrawer from "../NavDrawer";
 
 export default function Home() {
@@ -34,17 +34,58 @@ export default function Home() {
           >
             hacker | artist | activist | nerd
           </Typography>
-          <Typography variant="h1" className="major-mono" sx={{ letterSpacing: "10px"}}>
+          <Typography
+            variant="h1"
+            className="major-mono"
+          sx={{ letterSpacing: "10px", marginTop: { xs: "100px", md: "0"} }}
+          >
             Calico Seders
           </Typography>
         </Box>
       </Box>
 
-      <Box sx={{ display: "flex", justifyContent: "space-evenly", marginTop: "170px" }}>
-        <Link to="/about"><Avatar sx={{ width: 250, height: 250, border: "18px red dotted" }} alt="about" src={aboutImage} /></Link>
-        <Link to="/code"><Avatar sx={{ width: 250, height: 250, border: "18px red dotted" }} alt="code" src={codeImage} /></Link>
-        <Link to="/jazz"><Avatar sx={{ width: 250, height: 250, border: "18px red dotted" }} alt="jazz" src={jazzImage} /></Link>
-        <Link to="/love"><Avatar sx={{ width: 250, height: 250, border: "18px red dotted" }} alt="code" src={loveImage} /></Link>
+      <Box
+        sx={{
+          display: "flex",
+          marginTop: { xs: "60px", md: "170px" },
+          flexDirection: {
+            xs: "column",
+            md: "row",
+          },
+          justifyContent: {
+            md: "space-evenly",
+          },
+          alignItems: { xs: "center" }
+        }}
+      >
+        <Link to="/about">
+          <Avatar
+            sx={{ width: 250, height: 250, border: "18px red dotted", marginBottom: "30px" }}
+            alt="about"
+            src={aboutImage}
+          />
+        </Link>
+        <Link to="/code">
+          <Avatar
+            sx={{ width: 250, height: 250, border: "18px red dotted", marginBottom: "30px" }}
+            alt="code"
+            src={codeImage}
+          />
+        </Link>
+        <Link to="/jazz">
+          <Avatar
+            sx={{ width: 250, height: 250, border: "18px red dotted", marginBottom: "30px" }}
+            alt="jazz"
+            src={jazzImage}
+          />
+        </Link>
+        <Link to="/love">
+          <Avatar
+            sx={{ width: 250, height: 250, border: "18px red dotted", marginBottom: "30px" }}
+            alt="code"
+            src={loveImage}
+          />
+        </Link>
       </Box>
     </Box>
   );
