a0e60344a8221be83502ab723a208e4ac8cef8b9
kalikokalikova <calico.seders@gmail.com>
2025-12-14 16:32:10 -0800

    more responsiveness

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -12,83 +12,77 @@ export default function Home() {
     backgroundImage: `url(${backgroundImage})`,
     backgroundSize: "cover",
     backgroundPosition: "center",
-    backgroundRepeat: "no-repeat",
+    backgroundRepeat: "repeat",
     height: "100vh",
   };
 
   const linkStyle = {
-    width: 250, height: 250, border: "1px chartreuse solid", marginBottom: "30px"
-  }
+    width: 250,
+    height: 250,
+    border: "1px chartreuse solid",
+    marginBottom: "30px",
+  };
 
   return (
     <Box sx={backgroundStyle}>
       <NavDrawer />
 
-      <Box sx={{ display: "flex", justifyContent: "end" }}>
-        <Box
-          sx={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "flex-end",
-            margin: "10px",
-          }}
-        >
+      {/* HEADER TEXT */}
+      <Box
+        sx={{
+          backgroundColor: {
+            md: "blue",
+          },
+        }}
+      >
+        <Box>
           <Typography
             variant="h4"
-            sx={{ letterSpacing: "15px", textTransform: "uppercase" }}
+            sx={{
+              letterSpacing: "15px",
+              textTransform: "uppercase",
+              whiteSpace: "nowrap",
+              textAlign: { md: "end" },
+            }}
           >
             hacker | artist | activist | nerd
           </Typography>
           <Typography
             variant="h1"
-          sx={{ color: "darkred", letterSpacing: "10px", marginTop: { xs: "100px", md: "0"} }}
+            sx={{
+              color: "darkred",
+              letterSpacing: "10px",
+              whiteSpace: "nowrap",
+              fontSize: { xs: "18rem", md: "7rem" },
+              textAlign: "end",
+            }}
           >
             Calico Seders
           </Typography>
         </Box>
       </Box>
 
+      {/* LINKS */}
       <Box
         sx={{
           display: "flex",
-          marginTop: { xs: "60px", md: "170px" },
-          flexDirection: {
-            xs: "column",
-            md: "row",
-          },
-          justifyContent: {
-            md: "space-evenly",
-          },
-          alignItems: { xs: "center" }
+          flexDirection: {xs: "column", md: "row"},
+          marginTop: {xs: "-120px", md: "300px"},
+          alignItems: "center",
+          justifyContent: { xs: "space-evenly", md: "row"}
         }}
       >
         <Link to="/about">
-          <Avatar
-            sx={linkStyle}
-            alt="about"
-            src={aboutImage}
-          />
+          <Avatar sx={linkStyle} alt="about" src={aboutImage} />
         </Link>
         <Link to="/code">
-          <Avatar
-            sx={linkStyle}
-            alt="code"
-            src={codeImage}
-          />
+          <Avatar sx={linkStyle} alt="code" src={codeImage} />
         </Link>
         <Link to="/jazz">
-          <Avatar
-            sx={linkStyle}
-            alt="jazz"
-            src={jazzImage}
-          />
+          <Avatar sx={linkStyle} alt="jazz" src={jazzImage} />
         </Link>
         <Link to="/love">
-          <Avatar
-            sx={linkStyle}
-            alt="code"
-            src={loveImage}
-          />
+          <Avatar sx={linkStyle} alt="code" src={loveImage} />
         </Link>
       </Box>
     </Box>
